1c369fdf33b61887adc24b1afc1023976bc23490
Felipe Pereira <felipe.pereiran@ufrpe.br>
2026-06-02 21:24:15 -0300

    Update do código para melhor se encaixar com o metodo avaliativo da segunda VA

--- projetoweb/urls.py
+++ projetoweb/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -8,8 +7,16 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #path('polls/', include('polls.urls')),
     path('portfolio/', include('portfolio.urls')),
 
+    # API de Tarefas (da aula DRF)
+    path('api/tarefas/', include('tarefas.urls')),
+
+    # API do Perfil
+    path('', include('core.urls')),
+
+    # Autenticacao JWT
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
